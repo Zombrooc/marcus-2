f4e525b92de4481bf5d380575b6a412e7798c98e
Elian Valdez <elian.valdez09@gmail.com>
2023-11-22 22:24:30 -0300

    adasdasd

---
+++ lib/library.sql
@@ -0,0 +1,108 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 23/11/2023 às 02:18
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `library`
+--
+CREATE DATABASE library;
+
+USE library;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `bookName` varchar(255) NOT NULL,
+  `yearRelease` int(11) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `bookDescription` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `publisherId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `publisher`
+--
+
+CREATE TABLE `publisher` (
+  `id` int(11) NOT NULL,
+  `publisherName` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `publisher`
+--
+
+INSERT INTO `publisher` (`id`, `publisherName`) VALUES
+(57, 'Outra Editora'),
+(58, 'NOva Editra Elian Valdez');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `editora_id` (`publisherId`);
+
+--
+-- Índices de tabela `publisher`
+--
+ALTER TABLE `publisher`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de tabela `publisher`
+--
+ALTER TABLE `publisher`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `books`
+--
+ALTER TABLE `books`
+  ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`publisherId`) REFERENCES `publisher` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
