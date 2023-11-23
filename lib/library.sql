3251cee662e7c8b289cbe7315696c8767f00f4b8
Elian Valdez <elian.valdez09@gmail.com>
2023-11-22 22:34:22 -0300

    finished

--- lib/library.sql
+++ lib/library.sql
@@ -51,13 +51,6 @@ CREATE TABLE `publisher` (
   `publisherName` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Despejando dados para a tabela `publisher`
---
-
-INSERT INTO `publisher` (`id`, `publisherName`) VALUES
-(57, 'Outra Editora'),
-(58, 'NOva Editra Elian Valdez');
 
 --
 -- Índices para tabelas despejadas
